23d2be908755b9370b4dc8bdd83e96b5470fb87c
Tom Price <tom@codedinternet.com>
2015-09-30 15:09:55 +0100

    Change deploy script to and after script to fix issues with not adding files for commit.

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -21,7 +21,7 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]
   cd gh-pages
   git config user.email "webmaster@nottinghamtec.co.uk"
   git config user.name "tecbot"
-  git add -A
+  git add -A .
   git commit -a -m "Travis Build $TRAVIS_BUILD_NUMBER"
   git push --quiet origin gh-pages > /dev/null 2>&1 # Hiding all the output from git push command, to prevent token leak.
 fi
