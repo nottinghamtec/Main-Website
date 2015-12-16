fe985762693f3388143de5a2c30d85a9d4e2920e
Tom Price <tom@codedinternet.com>
2015-12-16 02:11:57 +0000

    Update compass config to use correct paths for everything

--- config.rb
+++ config.rb
@@ -5,9 +5,14 @@ require 'materialize-sass'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "static/css"
+http_stylesheets_path = "css"
 sass_dir = "src/sass"
 images_dir = "static/imgs"
+http_images_path = "imgs"
 javascripts_dir = "static/js"
+http_javascripts_path = "js"
+fonts_dir = "static/fonts"
+http_fonts_path = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
