8f87afb70df42b1aceeb5ebb1beb566dfc0d7d81
Harry Bridge <harrybridge01@gmail.com>
2016-03-29 22:18:02 +0100

    Reduce prep script to minimum

--- _bin/prep.sh
+++ _bin/prep.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# enable error reporting to the console
-set -e
-
-echo "Bundle path: $BUNDLE_PATH"
-echo "Pull Request: $TRAVIS_PULL_REQUEST"
-
 # If staging setup CNAME and SEO block
 if [[ $TRAVIS_PULL_REQUEST = "false" && $TRAVIS_BRANCH = "staging" ]]
   then
