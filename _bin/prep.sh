be6d06d832b4dbf200eef754fe8884884001cb58
Harry Bridge <harrybridge01@gmail.com>
2016-03-29 22:07:14 +0100

    Removed build number

--- _bin/prep.sh
+++ _bin/prep.sh
@@ -6,11 +6,6 @@ set -e
 echo "Bundle path: $BUNDLE_PATH"
 echo "Pull Request: $TRAVIS_PULL_REQUEST"
 
-if [[ "$TRAVIS_BUILD_NUMBER" ]]
-then
-  echo $TRAVIS_BUILD_NUMBER > _includes/travis_build_number.txt
-fi
-
 # If staging setup CNAME and SEO block
 if [[ $TRAVIS_PULL_REQUEST = "false" && $TRAVIS_BRANCH = "staging" ]]
   then
