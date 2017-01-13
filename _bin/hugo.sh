acdcc81ac9c421ab9d9a21f31d2a013a22314430
Harry Bridge <harry@harrybridge.co.uk>
2017-01-13 22:42:37 +0000

    Rename hugo calls to please @samozzy

--- _bin/hugo.sh
+++ _bin/hugo.sh
@@ -2,5 +2,5 @@
 
 mkdir ~/hugo && cd "$_"
 curl -Lk https://github.com/spf13/hugo/releases/download/v0.17/hugo_0.17_Linux-64bit.tar.gz | tar xz
-mv hugo*/hugo* ~/hugo018 && chmod +x "$_"
+mv hugo*/hugo* ~/hugo017 && chmod +x "$_"
 exit 0
