e441743210f0cc412168b31e24526181841be21c
Johnathan Graydon <johnathan99j@yahoo.com>
2017-10-15 11:16:50 +0100

    Bump Hugo to v29

--- _bin/hugo.sh
+++ _bin/hugo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir ~/hugo && cd "$_"
-curl -Lk https://github.com/spf13/hugo/releases/download/v0.17/hugo_0.17_Linux-64bit.tar.gz | tar xz
-mv hugo*/hugo* ~/hugo017 && chmod +x "$_"
+curl -Lk https://github.com/spf13/hugo/releases/download/v0.29/hugo_0.29_Linux-64bit.tar.gz | tar xz
+mv hugo ~/hugo029 && chmod +x "$_"
 exit 0
