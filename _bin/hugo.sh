18f5af7aa5a9794bda6a2f46c91e5db412b718d2
Arona Jones <aj@aronajones.com>
2020-06-27 20:42:48 +0100

    Another travis update

--- _bin/hugo.sh
+++ _bin/hugo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir ~/hugo && cd "$_"
-curl -Lk https://github.com/spf13/hugo/releases/download/v0.73/hugo_0.73_Linux-64bit.tar.gz | tar xz
+curl -Lk https://github.com/gohugoio/hugo/releases/download/v0.73.0/hugo_0.73.0_Linux-64bit.tar.gz | tar xz
 mv hugo ~/hugo073 && chmod +x "$_"
 exit 0
