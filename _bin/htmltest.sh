5316007b0986706ee769f2cfa56cc1c6918c9d4a
Arona Jones <aj@aronajones.com>
2021-08-05 12:56:50 +0100

    Try switch to github actions for test/build/deploy
    Peaceiris, you are awesome.

--- _bin/htmltest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -Lk https://github.com/wjdp/htmltest/releases/download/v0.4.1/htmltest-linux -o ~/htmltest && chmod +x "$_"
-exit 0
--- _bin/hugo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir ~/hugo && cd "$_"
-curl -Lk https://github.com/gohugoio/hugo/releases/download/v0.73.0/hugo_0.73.0_Linux-64bit.tar.gz | tar xz
-mv hugo ~/hugo073 && chmod +x "$_"
-exit 0
